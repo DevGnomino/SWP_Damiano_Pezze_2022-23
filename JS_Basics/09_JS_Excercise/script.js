54a6c41c24da65a38abd4e1d216390d9953ebdc0
DevGnomino <damianopezze@gmail.com>
2022-10-13 09:59:18 +0200

    JS for-loop excercise 1

--- JS_Basics/09_JS_Excercise/script.js
+++ JS_Basics/09_JS_Excercise/script.js
@@ -4,11 +4,36 @@
 // X X X X 
 // O O O O 
 
-// 2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.) 
+for (let i = 0; i < 4; i++) {
+    if (i % 2 == 0) {
+        console.log("X X X X");
+    } else {
+        console.log("O O O O");
+    }
+}
+console.log("------------");
 
-// 3. Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.: 
+
+// 2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
+let result1 = 0;
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        result1 += i;
+    }
+}
+console.log(result1);
+console.log("------------");
+
+let result2 = 0;
+for (let i = 0; i <= 100; i= i + 2) {
+    result2 += i;
+}
+console.log(result2);
+
+
+// 3. Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.:
 // XXXX
 // X   X
-// X   XX      
-// X   X   
+// X   XX
+// X   X
 // XXXX
