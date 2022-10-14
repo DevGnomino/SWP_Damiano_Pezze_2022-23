afb2cfd07e3adff9675f22f92a5d8dd51e1bbb4b
DevGnomino <damianopezze@gmail.com>
2022-10-14 15:26:57 +0200

    minor refinements random num generator

--- JS&HTML/02_Zufallsgenerator/script.js
+++ JS&HTML/02_Zufallsgenerator/script.js
@@ -1,7 +1,3 @@
-
-
-Math.round(Math.random() * 70); //returns random number from 0 to 70
-
 function generate() {
     let min = parseInt(document.getElementById("min").value);
     let max = parseInt(document.getElementById("max").value);
@@ -9,7 +5,6 @@ function generate() {
 
     let number = Math.round(Math.random() * (max - min) + min);
     console.log(number);
-
 
     document.getElementById("result").innerHTML = number;
 }
